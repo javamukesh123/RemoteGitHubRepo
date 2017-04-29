8fba62d3355ea31a7bd13339e6bce97b19620afd
mukeshTL <TL@gmail.com>
2017-04-30 01:31:56 +0530

    TL create Project(GitHubTLTest1) and kept in Remote repository..

---
+++ GITProject1/src/com/nt/github/GitHubTLTest1.java
@@ -0,0 +1,11 @@
+package com.nt.github;
+
+public class GitHubTLTest1 {
+
+	public static void main(String[] args) {
+		System.out.println("this is my git app1");
+		System.out.println("this is my git app2");
+
+	}
+
+}
